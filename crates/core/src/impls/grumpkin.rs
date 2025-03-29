1e950ccabe1c983e9a51308d55eb9916492d01d0
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-30 01:55:19 +0530

    feat: grumpkin bsgs

--- crates/core/src/impls/grumpkin.rs
+++ crates/core/src/impls/grumpkin.rs
@@ -1,6 +1,56 @@
+use ark_grumpkin::{Affine, Fr, Projective};
+
+use std::collections::HashMap;
+use std::hash::Hash;
+
+use crate::BabyGiantOps;
+
+#[derive(Hash, Clone, PartialEq, Eq)]
+pub struct GrumpkinBabyGiant {
+    steps_count: u64,
+}
+
+/// Implementation for u128 modular exponentiation
+impl BabyGiantOps for GrumpkinBabyGiant {
+    type El = Projective;
+    type Scalar = u64;
+
+    fn steps_count(&self) -> Self::Scalar {
+        1_048_576 // 2 ** 20
+    }
+
+    fn baby_steps(&self, base: &Self::El) -> HashMap<Self::El, u64> {
+        let mut baby_steps = HashMap::new();
+        let mut current = *base;
+
+        let mut baby_step = 0;
+        while baby_step < self.steps_count {
+            baby_step += 1;
+            baby_steps.insert(current, baby_step);
+            current = current + base;
+        }
+
+        baby_steps
+    }
+
+    fn el_operation(&self, lhs: &Self::El, rhs: &Self::El) -> Self::El {
+        lhs + rhs
+    }
+
+    fn gaint_step_jump(&self, base: &Self::El) -> Self::El {
+        let m: Fr = self.steps_count.into();
+        -(*base * m)
+    }
+
+    fn process_result(&self, baby: &u64, giant: &u64) -> u64 {
+        let step_count = self.steps_count;
+        giant * step_count + baby
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use ark_grumpkin::{Affine, Fr, GrumpkinConfig, Projective, G_GENERATOR_X, G_GENERATOR_Y};
+    use ark_grumpkin::{Affine, Fr, Projective, G_GENERATOR_X, G_GENERATOR_Y};
     use std::time::Instant;
 
     #[test]

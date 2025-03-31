ea09836e516251a3dba675916bd4dc4db2d4cd9d
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-31 13:23:24 +0530

    wasm-pack init

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,6 +1,13 @@
+mod utils;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm!");
 }
